6a9f83dd4f5f356b49e222da448b25ae911bb9dd
cwjki <cwjki97@gmail.com>
2023-03-25 17:11:36 -0400

    adding the user histories from 1 to 6

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-
 export default function Calculator() {
   return (
     <div className="container-fluid">
       <div className="row align-items-center vh-100">
         <div className="col-lg-4 col-md-6 col-sm-8 col-11 mx-auto">
-          <div className="card rounded-5 pt-4 pb-4 px-2 bg-dark shadow-lg">
+          <div className="card rounded-5 pb-4 px-2 bg-dark shadow-lg mt-3">
             <div className="card-body text-center mt-5">
               <div className="card-text text-end fs-6 text-secondary">
                 56 x 8
               </div>
-              <div className="card-title text-end fs-1 text-info">456</div>
+              <div id="display" className="card-title text-end fs-1 text-info">456</div>
 
               <div className="row align-items-center gap-2 mt-4 px-1">
                 <button
-                  id=""
+                  id="clear"
                   type="button"
                   className="btn btn-secondary col rounded-4 p-3 fs-3"
                 >
@@ -35,7 +33,7 @@ export default function Calculator() {
                   %
                 </button>
                 <button
-                  id=""
+                  id="divide"
                   type="button"
                   className="btn btn-warning col rounded-4 p-3 fs-3"
                 >
@@ -45,28 +43,28 @@ export default function Calculator() {
 
               <div className="row align-items-center gap-2 mt-2 px-1">
                 <button
-                  id=""
+                  id="seven"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
                   7
                 </button>
                 <button
-                  id=""
+                  id="eight"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
                   8
                 </button>
                 <button
-                  id=""
+                  id="nine"
                   type="button"
                   className="btn btn-light  col rounded-4 p-3 fs-3"
                 >
                   9
                 </button>
                 <button
-                  id=""
+                  id="multiply"
                   type="button"
                   className="btn btn-warning col rounded-4 p-3 fs-3"
                 >
@@ -76,28 +74,28 @@ export default function Calculator() {
 
               <div className="row align-items-center gap-2 mt-2 px-1">
                 <button
-                  id=""
+                  id="four"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
-                  1
+                  4
                 </button>
                 <button
-                  id=""
+                  id="five"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
-                  2
+                  5
                 </button>
                 <button
-                  id=""
+                  id="six"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
-                  3
+                  6
                 </button>
                 <button
-                  id=""
+                  id="subtract"
                   type="button"
                   className="btn btn-warning col rounded-4 p-3 fs-3"
                 >
@@ -107,21 +105,52 @@ export default function Calculator() {
 
               <div className="row align-items-center gap-2 mt-2 px-1">
                 <button
-                  id=""
+                  id="one"
+                  type="button"
+                  className="btn btn-light col rounded-4 p-3 fs-3"
+                >
+                  1
+                </button>
+                <button
+                  id="two"
+                  type="button"
+                  className="btn btn-light col rounded-4 p-3 fs-3"
+                >
+                  2
+                </button>
+                <button
+                  id="three"
+                  type="button"
+                  className="btn btn-light col rounded-4 p-3 fs-3"
+                >
+                  3
+                </button>
+                <button
+                  id="add"
+                  type="button"
+                  className="btn btn-warning col rounded-4 p-3 fs-3"
+                >
+                  +
+                </button>
+              </div>
+
+              <div className="row align-items-center gap-2 mt-2 px-1">
+                <button
+                  id="zero"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
                   0
                 </button>
                 <button
-                  id=""
+                  id="decimal"
                   type="button"
                   className="btn btn-light col rounded-4 p-3 fs-3"
                 >
-                  ,
+                  .
                 </button>
                 <button
-                  id=""
+                  id="equals"
                   type="button"
                   className="btn btn-warning col-6 rounded-4 p-3 fs-3"
                 >
@@ -132,7 +161,7 @@ export default function Calculator() {
           </div>
 
           {/* Footer */}
-          <nav className="navbar pb-2 mt-3">
+          <nav className="navbar">
             <div className="container-fluid justify-content-center">
               <span className="navbar-brand fs-6 text-dark fw-bold">
                 by cwjki
